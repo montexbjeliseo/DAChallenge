88bf78efbc1111988434fa53e2c2850a4a936f16
montexbjeliseo <secondmtx@gmail.com>
2023-07-26 19:36:36 -0300

    Coord list

--- weather_api.py
+++ weather_api.py
@@ -4,23 +4,29 @@ import json
 from datetime import datetime
 import pandas as pd
 
-def request_data_by_city(city: str):
+
+def request_data_by_city(city: str = None, coord: str = None):
     """
     Recibe "city" que es el nombre de la ciudad, en formato string
     Hace una solicitud a la api y si el código de respuesta
     Es 200, devuelve la data en formato json
     De lo contrario, intentará manejar las excepciones
     """
-    print("*"*50)
+    print("*" * 50)
     print(f"Solicitando datos")
-    print(f"Ciudad: \"{city}\"")
-    print(f"Unidad: \"{config.UNITS}\"")
-    
-    params = {"q": city, "units": config.UNITS, "appid": config.API_KEY}
+    print(f'Ciudad: "{city}"')
+    print(f'Unidad: "{config.UNITS}"')
 
     try:
-        
-        response = requests.get(config.BASE_URL, params)
+        if city:
+            params = {"q": city, "units": config.UNITS, "appid": config.API_KEY}
+
+            response = requests.get(config.BASE_URL, params)
+        else:
+            response = requests.get(
+                f"{config.BASE_URL}?{coord}&appid={config.API_KEY}&units{config.UNITS}"
+            )
+
         response.raise_for_status()
 
         if response.status_code == 200:
@@ -32,39 +38,43 @@ def request_data_by_city(city: str):
     except Exception as e:
         print(f"Error desconocido: {e}")
 
+
 def export_to_json(data: list, filename: str):
     """Exportar datos a un archivo JSON."""
-    with open(filename, 'w') as file:
+    with open(filename, "w") as file:
         json.dump(data, file)
-        
+
+
 def save_to_file(data, filename):
-    with open(filename, 'w') as file:
+    with open(filename, "w") as file:
         file.write(data)
         file.close()
 
+
 def formatted_date_time():
     return datetime.now().strftime("%Y%m%d")
-        
+
+
 def export_to_csv(data_list, filename):
-    
     dfs = []
     for data in data_list:
-        wdf = pd.json_normalize(data['weather'][0]).add_prefix('weather_')
-        
-        keys_to_remove = ['weather', 'base', 'timezone', 'id', 'name', 'cod']
+        wdf = pd.json_normalize(data["weather"][0]).add_prefix("weather_")
+
+        keys_to_remove = ["weather", "base", "timezone", "id", "cod"]
 
         for key in keys_to_remove:
             del data[key]
-        
+
         dfc = pd.json_normalize(data)
-        
+
         dfc = pd.concat([dfc, wdf], axis=1)
-        
+
         dfs.append(dfc)
-        
+
     df = pd.concat(dfs, axis=0)
-    
+
     df.to_csv(filename, index=False)
+
 
 def get_cities_weather_data():
     """
@@ -72,28 +82,39 @@ def get_cities_weather_data():
     A partir de la lista de ciudades
     Devuelve un conjunto de respuestas
     """
-    
+
     # Acá almacenamos los datos de todas las ciudades
     # Para luego ser exportadas
     cities_data = []
     unreached_cities = []
-    
+
     for city in config.cityList:
-        city_data = request_data_by_city(city)
+        city_data = request_data_by_city(city=city)
         if city_data:
             cities_data.append(city_data)
         else:
             unreached_cities.append(city)
-    
-    print("_"*50)
-    
-    unreached_cities_filename = f'{config.FOLDER}/no_alcanzadas_{formatted_date_time()}.lst'
-    weather_data_filename = f'{config.FILENAME}_{formatted_date_time()}.csv'
-    
-    export_to_json(unreached_cities, unreached_cities_filename)    
-    print(f"Se guardó la lista de ciudades no alcanzadas en {unreached_cities_filename}")
-    
+
+    for coord in config.coordList:
+        coord_data = request_data_by_city(coord=coord)
+        if coord_data:
+            cities_data.append(coord_data)
+        else:
+            unreached_cities.append(coord)
+
+    print("_" * 50)
+
+    unreached_cities_filename = (
+        f"{config.FOLDER}/no_alcanzadas_{formatted_date_time()}.lst"
+    )
+    weather_data_filename = f"{config.FILENAME}_{formatted_date_time()}.csv"
+
+    export_to_json(unreached_cities, unreached_cities_filename)
+    print(
+        f"Se guardó la lista de ciudades no alcanzadas en {unreached_cities_filename}"
+    )
+
     export_to_csv(cities_data, weather_data_filename)
     print(f"Los datos del tiempo se guardaron en {weather_data_filename}")
-            
+
     return cities_data
